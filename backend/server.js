29bc4103be4ff579b64f9209a2aa2c5d252b0eb3
Yan Kaliada <vetkaykropa@gmail.com>
2024-11-22 12:45:54 +0300

    added vote page and api

--- backend/server.js
+++ backend/server.js
@@ -7,26 +7,46 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Временное хранилище фильмов
-let movies = [];
+// Предустановленные фильмы
+let movies = [
+    { id: 1, title: 'Интерстеллар', votes: 0 },
+    { id: 2, title: 'Начало', votes: 0 },
+    { id: 3, title: 'Матрица', votes: 0 },
+    { id: 4, title: 'Аватар', votes: 0 },
+    { id: 5, title: 'Темный рыцарь', votes: 0 },
+];
 
-// API маршруты
+// Получить список фильмов
 app.get('/movies', (req, res) => {
     res.json(movies);
 });
 
+// Добавить новый фильм
 app.post('/movies', (req, res) => {
-    const newMovie = { id: Date.now(), title: req.body.title };
+    const newMovie = { id: Date.now(), title: req.body.title, votes: 0 };
     movies.push(newMovie);
     res.json(newMovie);
 });
 
+// Удалить фильм
 app.delete('/movies/:id', (req, res) => {
     movies = movies.filter((movie) => movie.id !== parseInt(req.params.id, 10));
     res.status(204).end();
 });
 
-// Отдача статических файлов React
+// Голосование за фильм
+app.post('/movies/:id/vote', (req, res) => {
+    const movieId = parseInt(req.params.id, 10);
+    const movie = movies.find((movie) => movie.id === movieId);
+    if (movie) {
+        movie.votes += 1; // Увеличиваем число голосов
+        res.json(movie);
+    } else {
+        res.status(404).json({ message: 'Фильм не найден' });
+    }
+});
+
+// Обслуживание статических файлов React
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // Обработка всех остальных маршрутов и возврат index.html
