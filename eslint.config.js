226a87060f2e08120f5ae9896407b0fe4de1e802
Yan Kaliada <vetkaykropa@gmail.com>
2024-12-10 21:35:29 +0300

    6

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,12 @@
 export default {
-  env: {
-    node: true,
-    es2021: true,
+  languageOptions: {
+    globals: {
+      process: 'readonly',
+      __dirname: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2021,
+    },
   },
   rules: {
     'no-console': 'warn',
