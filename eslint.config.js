a2377aa20d9cc1ceb7310013c0d34bd471389d6a
Yan Kaliada <vetkaykropa@gmail.com>
2024-12-10 21:26:31 +0300

    2

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ["eslint:recommended"],
+  env: {
+    node: true,
+    es2021: true
+  },
+  rules: {
+    "no-console": "warn",
+    "no-unused-vars": "warn"
+  }
+};
