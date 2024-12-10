af51d72d1bda963c426e07a7999f1bfd9b2278f1
Yan Kaliada <vetkaykropa@gmail.com>
2024-12-10 21:42:12 +0300

    switche requared to import

--- tests/apiTests/getApi.test.js
+++ tests/apiTests/getApi.test.js
@@ -1,9 +1,8 @@
-const { test, expect } = require('@playwright/test');
-const getMovies = require('./utils/apiUtils');
-const validateMovie = require('./utils/validateMovie');
+import { test, expect } from '@playwright/test';
+import getMovies from './utils/apiUtils';
+import validateMovie from './utils/validateMovie';
 
 test('Проверка гет запроса api/movies', async ({ request }) => {
-
 
   let response;
   let movies;
@@ -14,17 +13,14 @@ test('Проверка гет запроса api/movies', async ({ request }) =>
     movies = result.movies;
   });
 
-
   await test.step('Проверка статус-кода', () => {
     expect(response.status()).toBe(200);
   });
-
 
   await test.step('Проверка что массив не пустой и он есть', () => {
     expect(Array.isArray(movies)).toBeTruthy();
     expect(movies.length).toBeGreaterThan(0);
   });
-
 
   await test.step('Проверка ключей у каждого фильма', () => {
     movies.forEach(movie => validateMovie(movie));
